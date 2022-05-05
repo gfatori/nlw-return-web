bc5fa1d6d564e65f6a600b8c3180ff9607f0b38a
Gabriel Fatori <gabriel.fatori@gmail.com>
2022-05-04 22:38:10 -0300

    Finished front-end, WIP backend

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -5,6 +5,7 @@ import suggestionImageUrl from '../Assets/Thought.svg';
 import { useDebugValue, useState } from "react";
 import { FeedbackChooseTypeStep } from "./WidgetForm/Steps/FeedbackChooseTypeStep";
 import { FeedbackContentStep } from "./WidgetForm/Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./WidgetForm/Steps/FeedbackSuccessStep";
 
 export const feedbackTypes = {
     BUG: {
@@ -34,26 +35,35 @@ export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm(): JSX.Element {
     const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+    const [feedbackSent, setFeedbackSent] = useState(false);
 
     function handleRestartFeedback() {
         setFeedbackType(null);
+        setFeedbackSent(false);
     }
 
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-            {!feedbackType ? (
+            { feedbackSent ? (
+                <FeedbackSuccessStep onFeedbackRestartRequested={handleRestartFeedback}/>
+
+            ) : (
+                <>
+                {!feedbackType ? (
                 <FeedbackChooseTypeStep onFeedbackTypeChange={setFeedbackType} />
             ) : (
-                <FeedbackContentStep feedbackType={feedbackType}
-                onFeedbackRestartRequested={handleRestartFeedback} 
+                <FeedbackContentStep
+                 feedbackType={feedbackType}
+                 onFeedbackRestartRequested={handleRestartFeedback} 
+                 onFeedbackSent={() => setFeedbackSent(true)}
                 />
+            )}
+                </>
             )}
 
             <footer className="text-xs text-neutral-400">
-                Feito com ♥ pelo <a className="underline underline-offset-2" href="https://rocketseat.com.br/"> Gabriel Fatori </a>
+                Feito com ♥ por <a className="underline underline-offset-2" href="https://rocketseat.com.br/"> Gabriel Fatori </a>
             </footer>
         </div>
     );
 }
-{/* <span className="text-xl leading-6"> Deixe seu feedback</span>
-<CloseButton /> */}
